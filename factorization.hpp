320ea4b82704a6cd8b390f05eb540ca099888583
DenMusya <denis0000097@gmail.com>
2022-10-17 00:44:19 +0300

    V2

--- factorization.hpp
+++ factorization.hpp
@@ -1,3 +1,29 @@
-int foo(int a, int b) {
-    return a + b;
+#include <iostream>
+#include <map>
+#include <vector>
+
+bool Eratosfen[10000000];
+
+void fact(long long a) {
+  std::map<int, int> mult;
+  std::vector<long long> primes;
+  for (int i = 2; i < 1000000; i++) {
+    if (!Eratosfen[i]) {
+      primes.push_back(i);
+      for (int j = i + i; j <= 1000000; j += i) {
+        Eratosfen[j] = true;
+      }
+    }
+  }
+  for (size_t i = 0; i < primes.size(); ++i) {
+    while (a % primes[i] == 0) {
+      ++mult[primes[i]];
+      a /= primes[i];
+    }
+  }
+  for (auto it = mult.begin(); it!= mult.end(); ++it) {
+    if (it != mult.begin()) std::cout << " * ";
+    std::cout << (*it).first << '^' << (*it).second;
+  }
+  std::cout << std::endl;
 }
